c6ac09d48dd9836495fd086559752cdaba44f899
Johnatan <johnatan.ivini@gmail.com>
2024-09-16 12:18:16 -0300

    Ajustar persistencia filtros e botões de ação

--- app/src/components/Todo/Todo.js
+++ app/src/components/Todo/Todo.js
@@ -5,28 +5,63 @@ import { TextField, Button, Box, Typography, List, Container } from '@mui/materi
 import Status from '../../enum/status';
 import ListaDeItens from './ListaDeItens';
 import Modal from '../Modal/Modal';
-import Tasks from '../../Persistencia/Tasks';
+import Tasks from '../../Service/Task';
+import Filter from '../Filter/Filter';
+import TaskFilters from '../../Service/TaskFilter';
+import DeleteIcon from '@mui/icons-material/DeleteForever'
 
-const taskModel = new Tasks();
+
 
 const validationSchema = Yup.object({
-  todo: Yup.string()
+   todo: Yup.string()
     .required('Titulo não pode ser vazio')
 });
 
 const Todo = () => {
+
+  const tasks = React.useMemo(() => new Tasks('todos'), []);
+  const taskFilter = React.useMemo(() => new TaskFilters('filter'), []);
+
   const [openDialog, setOpenDialog] = React.useState(false);
+  const [openDialogStatus, setOpenDialogStatus] = React.useState(false);
   const [todos, setTodos] = React.useState([]);
   const [editIndex, setEditIndex] = React.useState(null);
-
+  const [filtered, setFiltered] = React.useState([]);
 
    React.useEffect(() => {
-       setTodos(taskModel.list());
-   }, [todos])
+      let itens = tasks.list();
+      let filter = taskFilter.getFilter();
+      if (filter) {
+           let filtered = taskFilter.filter(filter.titulo, "titulo");
+           if (filter.order) {
+              filtered = taskFilter.orderBy(`${filter.titulo}`, "titulo", `${filter.order}`)
+           }
+          setFiltered(filtered);
+      } else {
+        setFiltered(itens)
+      }
+      setTodos(itens);
+   }, [todos, tasks, taskFilter]);
 
   const handleCloseDialog = () => {
-    setOpenDialog(false);
+    setOpenDialogStatus(false);
   };
+
+  const handleClearFilter = () => {
+     setFiltered(todos);
+     taskFilter.clearFilter();
+  }
+
+  const openDialogDeleteCompleted = () => {
+    setOpenDialogStatus(true);
+  }
+  const handleDeleteStatusCompleted = () => {
+      tasks.clearItemCompleted();
+      setOpenDialogStatus(false);
+  }
+  const closeDialogDeleteCompleted = () => {
+    setOpenDialogStatus(false);
+ }
 
   const formik = useFormik({
     initialValues: {
@@ -38,25 +73,23 @@ const Todo = () => {
         const updatedTodos = todos.map((todo, index) =>  {
           if (index === editIndex) {
             todos[editIndex].titulo = values.todo;
-              return todos[editIndex];
-             }
+            }
           return todo
         });
-        taskModel.setItem(updatedTodos);
+        tasks.setUpdateItem(updatedTodos);
         setEditIndex(null);
       } else {
-        debugger
-        todos.push({ titulo: values.todo, status: Status.pendente , data: new Date(), data_concluida: ''});
-        taskModel.setItem(todos);
+        let todo = { titulo: values.todo, status: Status.pendente , data: new Date(), data_concluida: ''};
+        tasks.addItem(todo);
       }
       resetForm();
     }
   });
 
   const handleStatus = (index) => {
-          let novasTodos = [...todos];
+          let novasTodos = todos;
           novasTodos[index].status = novasTodos[index].status === Status.pendente ? Status.concluido : Status.pendente;
-          taskModel.setItem(novasTodos);
+          tasks.setUpdateItem(novasTodos);
   }
 
   const handleEdit = (index) => {
@@ -68,54 +101,69 @@ const Todo = () => {
     setOpenDialog(true);
     setEditIndex(index);
   };
-
+ 
   const handleConfirmDialogDelete = () => {
-     taskModel.removeItem(editIndex)
-      setOpenDialog(false);
+    tasks.removeItem(editIndex)
+    setOpenDialog(false);
   }
 
   return (
     <Container component="main" maxWidth="false">
-      <Box sx={{ width: '100%', maxWidth: '100%', margin: 'auto', padding: 2 }}>
-        <Typography component="h1" variant="h5">
-          Tarefas
-        </Typography>
-        <Box
-          component="form"
-          onSubmit={formik.handleSubmit}
-          sx={{ mt: 1 }}
-        >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="todo"
-            label="Nova tarefa"
-            name="todo"
-            autoFocus
-            value={formik.values.todo}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={formik.touched.todo && Boolean(formik.errors.todo)}
-            helperText={formik.touched.todo && formik.errors.todo}
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{ mt: 2 }}
-          >
-            {editIndex !== null ? 'Atualizar Tarefa' : 'Adicionar Tarefa'}
-          </Button>
-        </Box>
-        <List sx={{ marginTop: 2, bgcolor: 'background.paper'}}>
-          {todos.map((todo, index) => (
-              <ListaDeItens todo={todo} index={index} handleStatus={handleStatus} handleEdit={handleEdit} handleDelete={handleDelete}/>
-          ))}
-        </List>
+      <Box sx={{
+          display :'grid',
+          gridTemplateColumns : '1fr 4fr',
+          justifyItems: 'justify-content',
+        }} >
+          <Filter handleClearFilter={handleClearFilter} />
+          <Box sx={{
+              border: '1px solid #ccc',
+              padding: '10px',
+          }}>
+            <Typography component="h1" variant="h5">
+              Tarefas
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={formik.handleSubmit}
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="todo"
+                label="Nova tarefa"
+                name="todo"
+                autoFocus
+                value={formik.values.todo}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                error={formik.touched.todo && Boolean(formik.errors.todo)}
+                helperText={formik.touched.todo && formik.errors.todo}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+              >
+                {editIndex !== null ? 'Atualizar Tarefa' : 'Adicionar Tarefa'}
+              </Button>
+            </Box>
+            <List sx={{ marginTop: 2, bgcolor: 'background.paper'}}>
+              {filtered.map((todo, index) => (
+                  <ListaDeItens key={index} todo={todo} index={index} handleStatus={handleStatus} handleEdit={handleEdit} handleDelete={handleDelete}/>
+              ))}
+            </List>
+            <Box component="footer" sx={{width:"100%", textAlign: "right", borderTop: "solid 1px #cccccc "}}>
+                  <Button variant="contained" color="secondary" sx={{mt:2}} onClick={() => openDialogDeleteCompleted()}> <DeleteIcon/>Remover Concluídas</Button>
+            </Box>
+          </Box>
+          
       </Box>
       <Modal titleModal = "Atenção" textModal = "Deseja remover este item da lista" openDialog={openDialog} handleCloseDialog= {handleCloseDialog} handleConfirm={handleConfirmDialogDelete} />
+      <Modal titleModal = "Atenção" textModal = "Todos os Itens como concluídos serão removidos!" openDialog={openDialogStatus} handleCloseDialog= {closeDialogDeleteCompleted} handleConfirm={handleDeleteStatusCompleted} />
     </Container>
   );
 };
